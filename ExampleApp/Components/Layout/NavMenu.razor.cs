954e1c7d0b1cefbd73af0ec2d751fef2cb29440a
Peter Liljeholm <peter.liljeholm@gmail.com>
2024-06-13 11:26:33 +0200

    feat(navmenu): added support for navitems using code behind

---
+++ ExampleApp/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,74 @@
+﻿using ExampleApp.Components.Shared;
+using ExampleApp.Resources.Languages;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.FluentUI.AspNetCore.Components;
+using NavLink = ExampleApp.Components.Shared.NavLink;
+
+namespace ExampleApp.Components.Layout;
+
+public partial class NavMenu
+{
+    public IReadOnlyList<NavItem> NavMenuItems { get; private set; } = [];
+
+    public NavMenu()
+    {
+        InitNavMenu();
+    }
+
+    public NavMenu InitNavMenu()
+    {
+        NavMenuItems =
+        [
+            new NavLink(
+                href: "/",
+                match: NavLinkMatch.All,
+                icon: new Icons.Regular.Size24.Home(),
+                title: AppResource.Home
+            ),
+
+            new NavGroup(
+                icon: new Icons.Regular.Size24.Rocket(),
+                title: AppResource.ExpandedMenuGroup,
+                expanded: true,
+                gap: "10px",
+                children:
+                [
+                    new NavLink(
+                        href: "/counter",
+                        icon: new Icons.Regular.Size24.Calculator(),
+                        title: AppResource.Counter
+                    ),
+
+                    new NavLink(
+                        href: "/weather",
+                        icon: new Icons.Regular.Size24.Cloud(),
+                        title: AppResource.Weather
+                    ),
+
+                    new NavGroup(
+                        icon: new Icons.Regular.Size24.SettingsCogMultiple(),
+                        title: AppResource.FoldedMenuGroup,
+                        expanded: false,
+                        gap: "10px",
+                        children:
+                        [
+                            new NavLink(
+                            href: "/counter",
+                            icon: new Icons.Regular.Size24.Calculator(),
+                            title: AppResource.Counter
+                            ),
+
+                            new NavLink(
+                                href: "/weather",
+                                icon: new Icons.Regular.Size24.Cloud(),
+                                title: AppResource.Weather
+                            ),
+                        ]
+                    )
+                ]
+            )
+        ];
+
+        return this;
+    }
+}
---
+++ ExampleApp/Components/Shared/NavMenuItem.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace ExampleApp.Components.Shared;
+
+public partial class NavMenuItem
+{
+}
+
+public abstract record NavItem
+{
+    public string Title { get; init; } = string.Empty;
+    public string? Href { get; init; }
+    public NavLinkMatch Match { get; init; } = NavLinkMatch.Prefix;
+    public Icon Icon { get; init; } = new Icons.Regular.Size20.Document();
+}
+
+public record NavLink : NavItem
+{
+    public NavLink(string? href, Icon icon, string title, NavLinkMatch match = NavLinkMatch.Prefix)
+    {
+        Href = href;
+        Icon = icon;
+        Title = title;
+        Match = match;
+    }
+}
+public record NavGroup : NavItem
+{
+    public bool Expanded { get; init; }
+    public string Gap { get; init; }
+    public IReadOnlyList<NavItem> Children { get; }
+
+    public NavGroup(Icon icon, string title, bool expanded, string gap, List<NavItem> children)
+    {
+        Href = null;
+        Icon = icon;
+        Title = title;
+        Expanded = expanded;
+        Gap = gap;
+        Children = children.AsReadOnly();
+    }
+}
